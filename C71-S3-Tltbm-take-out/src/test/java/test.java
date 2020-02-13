9dd3e5286416db57ce0c64d3245809c9e7881447
DELL <DELL@DESKTOP-08DVM5D>
2020-02-13 22:43:50 +0800

    向数据库插入一条电话为：13135187907 密码为：123456（密码进行看不可逆加密） 的测试方法运行后可方便艳君做登陆数据验证

--- C71-S3-Tltbm-take-out/src/test/java/test.java
+++ C71-S3-Tltbm-take-out/src/test/java/test.java
@@ -1,12 +1,24 @@
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
+import javax.annotation.Resource;
+
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.yc.spirngboot.takeout.C71S3PljqSpringbootTakeoutApplication;
+import com.yc.spirngboot.takeout.bean.User;
+import com.yc.spirngboot.takeout.biz.UserBiz;
 
 import Decoder.BASE64Encoder;
 
-public class test {
 
+@SpringBootTest(classes =C71S3PljqSpringbootTakeoutApplication.class)// 就是你springboot的启动类
+public class test {
+	@Resource
+	private  UserBiz ubiz;
+	
 	public  String encodeByMd5(String encodeText){
 		try{
 		//创建md5的加密方式
@@ -25,6 +37,12 @@ public class test {
 }
 	@Test
 	public void testencodeByMd5() {
+		User user =new User();
+		user.setPhone("13135187907");
+		user.setPwd("123456");
+		
+		ubiz.reg(user);
+		
 		String code="123";
 		String result=encodeByMd5(code);	
 		System.out.println("code========="+code);
