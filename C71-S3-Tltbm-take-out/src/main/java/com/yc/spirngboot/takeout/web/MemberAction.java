3704df32be251fe1cb4ff90dd72860671786014f
Snail902 <55265231+Snail902@users.noreply.github.com>
2020-02-22 11:02:07 +0800

    11

--- C71-S3-Tltbm-take-out/src/main/java/com/yc/spirngboot/takeout/web/MemberAction.java
+++ C71-S3-Tltbm-take-out/src/main/java/com/yc/spirngboot/takeout/web/MemberAction.java
@@ -13,7 +13,7 @@ public class MemberAction {
 
     public static void main(String[] args) {
 		System.out.println("账号管理");
-		System.out.println("222");
+		System.out.println("211");
 		System.out.println("111");
 	}
 }
