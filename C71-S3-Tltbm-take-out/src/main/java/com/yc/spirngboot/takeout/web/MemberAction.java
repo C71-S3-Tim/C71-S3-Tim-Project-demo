d031011eb4f3a43f3293afb2253c02c1c4fb06b9
pc <pc@LAPTOP-D2H8A9UE>
2020-02-22 10:17:34 +0800

    账号

--- C71-S3-Tltbm-take-out/src/main/java/com/yc/spirngboot/takeout/web/MemberAction.java
+++ C71-S3-Tltbm-take-out/src/main/java/com/yc/spirngboot/takeout/web/MemberAction.java
@@ -1,10 +1,17 @@
 package com.yc.spirngboot.takeout.web;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.ui.Model;
+
+import com.yc.spirngboot.takeout.bean.User;
+import com.yc.spirngboot.takeout.biz.BizExcption;
+import com.yc.spirngboot.takeout.vo.Result;
+import com.yc.spirngboot.takeout.vo.encodeByMd5;
+
 public class MemberAction {
 
-	public static void main(String[] args) {
-		
+    public static void main(String[] args) {
+		System.out.println("账号管理");
 	}
-	
-	
 }
